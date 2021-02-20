623591412087b627215999978954859b1461b0d6
Alec Gibson <alec@alecgibson.ca>
2021-02-20 12:05:50 -0500

    Added sample code for interacting with discord slash commands

--- cmd/rex/main.go
+++ cmd/rex/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -10,13 +11,17 @@ import (
 	"github.com/root-of-a-tree/remote_exec_bot/pkg/config"
 )
 
+const guildID = "810610920313061428"
+
 func main() {
 	config, err := config.InitConfig()
 	if err != nil {
 		fmt.Println("error initializing bot configuration,", err)
 		return
 	}
-	fmt.Printf("Initialized config: %#v\n", config)
+	fmt.Println("Initialized config.")
+	fmt.Printf("Servers: %+v\n", config.Servers)
+	fmt.Printf("Scripts: %+v\n", config.Scripts)
 
 	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + config.Token)
@@ -25,21 +30,75 @@ func main() {
 		return
 	}
 
-	// TODO: use slash command!
-	// dg.ApplicationCommandCreate(appID, guildID, cmd)
+	// TODO: create application command
+	// commands := []*discordgo.ApplicationCommand{
+	// 	{
+	// 		Name:        "example",
+	// 		Description: "description",
+	// 	},
+	// 	{
+	// 		Name:        "options",
+	// 		Description: "example of options",
+	// 		Options: []*discordgo.ApplicationCommandOption{
+	// 			{
+	// 				Type:        discordgo.ApplicationCommandOptionString,
+	// 				Name:        "string-option",
+	// 				Description: "string option",
+	// 				Required:    true,
+	// 			},
+	// 		},
+	// 	},
+	// }
 
-	// Register the messageCreate func as a callback for MessageCreate events.
-	dg.AddHandler(messageCreate)
+	// TODO: create a handler for each application command
+	// commandHandlers := map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+	// 	"example": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	// 		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+	// 			Type: discordgo.InteractionResponseChannelMessageWithSource,
+	// 			Data: &discordgo.InteractionApplicationCommandResponseData{
+	// 				Content: "This is an example slash command",
+	// 			},
+	// 		})
+	// 	},
+	// 	"options": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	// 		stringOpt := i.Data.Options[0].IntValue()
+	// 		msgformat :=
+	// 			` Option entered:
+	// 		> string-option: %s
+	// `
+	// 		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+	// 			Type: discordgo.InteractionResponseChannelMessageWithSource,
+	// 			Data: &discordgo.InteractionApplicationCommandResponseData{
+	// 				Content: fmt.Sprintf(msgformat, stringOpt),
+	// 			},
+	// 		})
+	// 	},
+	// }
 
-	// In this example, we only care about receiving message events.
-	dg.Identify.Intents = discordgo.IntentsGuildMessages
+	// TODO: add all the handlers using discordgo.InteractionCreate as the event handler type
+	// dg.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	// 	if handler, ok := commandHandlers[i.Data.Name]; ok {
+	// 		handler(s, i)
+	// 	}
+	// })
+
+	dg.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Println("Rex is ready!")
+	})
 
 	// Open a websocket connection to Discord and begin listening.
 	err = dg.Open()
 	if err != nil {
-		fmt.Println("error opening connection,", err)
-		return
+		log.Fatalf("Cannot open the session: %v", err)
 	}
+
+	// TODO: create all our application commands
+	// for _, v := range commands {
+	// 	_, err := dg.ApplicationCommandCreate(appID, guildID, v)
+	// 	if err != nil {
+	// 		log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+	// 	}
+	// }
 
 	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
@@ -47,6 +106,7 @@ func main() {
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
+	log.Println("Gracefully shutting down")
 	// Cleanly close down the Discord session.
 	dg.Close()
 }
